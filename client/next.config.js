30c78e68180b0bedc3b993eacc59481e190516e8
ezenbhy <ezenbhy@gmail.com>
2022-11-14 15:46:27 +0900

    second

---
+++ client/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["www.gravatar.com","localhost",],
+  },
+  swcMinify: true,
+}
+
+module.exports = nextConfig
